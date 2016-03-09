5ff9581455c9ec1f59c6884455b81572656568ae
dcarraro <dcarraro@ciandt.com>
2016-03-09 11:34:58 -0300

    refactoring

--- src/main/java/redis/watch/RedisDAO.java
+++ src/main/java/redis/watch/RedisDAO.java
@@ -1,8 +1,6 @@
 package redis.watch;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -15,43 +13,41 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class RedisDAO {
 
-	// @Autowired
-	// @Qualifier("redisTemplate")
-	// private RedisTemplate<String, Model> template;
-
 	@Autowired
 	@Qualifier("stringRedisTemplate")
 	private StringRedisTemplate stringTemplate;
 
-	public void updateSKU(String sku, Integer qty) {
-
-		System.out.println("And now my watch begin!");
+	public String updateSKU(String sku, Integer qty) {
 
 		// execute a transaction
-		stringTemplate.execute(new SessionCallback<List<Object>>() {
+		List<Object> result = stringTemplate.execute(new SessionCallback<List<Object>>() {
+			@Override
 			public List<Object> execute(RedisOperations operations) throws DataAccessException {
-				operations.watch("key");
+
+				// Key to WATCH
+				operations.watch("sku:" + sku);
+
+				// Start Transaction
 				operations.multi();
 
-				Map<String, String> map = new HashMap<>();
-				map.put("name", "name2");
-				map.put("price", "2");
-
+				// Sleep to test WATCH
 				try {
+					System.out.println("And now my watch begin!");
 					Thread.sleep(30000);
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 
-				stringTemplate.opsForHash().putAll("key", map);
+				System.out.println("And now his watch is over!");
+
+				// Add SKU hash to Redis
+				stringTemplate.opsForHash().put("sku:" + sku, "qty", qty);
 
 				// This will contain the results of all ops in the transaction
 				return operations.exec();
 			}
 		});
 
-		System.out.println("And now his watch is over!");
-
+		return sku;
 	}
 }
